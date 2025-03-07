d09a5493a50dc16cfb81124ef8daef7a138aaee5
Kyle Johnson <tuneharmonic@gmail.com>
2025-03-07 17:49:14 -0600

    specify async action

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const core = require('@actions/core')
 const io = require('@actions/io')
 
-try {
-    const command = core.getInput('command');
-    console.log(`Finding command: ${command}`);
-
-    const path = await io.which(command, true);
-
-    console.log(`Found command: ${command} at path: ${path}`);
-    core.setOutput('path', path);
-} catch (error) {
-    core.setFailed(error.message)
+async function run() {
+    try {
+        const command = core.getInput('command');
+        console.log(`Finding command: ${command}`);
+    
+        const path = await io.which(command, true);
+    
+        console.log(`Found command: ${command} at path: ${path}`);
+        core.setOutput('path', path);
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
